5019105f8603f97bff8368d8d119cf5a542e353a
Netcafe96 <myblack532@gmail.com>
2025-06-09 09:13:57 +0700

    Update ai_image_utils.py

--- utils/ai_image_utils.py
+++ utils/ai_image_utils.py
@@ -1,1 +1,9 @@
+import openai, os
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
+def generate_prompt_from_timecode(t):
+    return f"An educational, realistic image representing a scene from a science video at {t} seconds."
+
+def generate_image(prompt):
+    r = openai.Image.create(prompt=prompt, n=1, size="512x512")
+    return r['data'][0]['url']
